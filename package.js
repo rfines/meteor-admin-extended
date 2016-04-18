1f6613b04812e24fa2f2b7d335ce1857c660991d
Robert Fines <fines.robert@gmail.com>
2016-04-18 12:43:08 -0500

    fixed global

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "robfines:meteor-admin-extended",
   summary: "A complete admin dashboard solution",
-  version: "1.0.6",
+  version: "1.0.7",
   git: "https://github.com/rfines/meteor-admin-extended.git"
 });
 
